52f2d9a991db87f31d3d33598178c663536798c2
Vu Ngoc Hong Phuc <phucvnh@gmail.com>
2018-04-13 16:48:08 +0700

    add modal, and backdrop UI

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -13,9 +13,19 @@ const controls = [
 const buildControls = (props) => {
     return (
         <div className={classes.BuildControls}> 
+            <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
             {controls.map( ctrl => { return (
-                <BuildControl key={ctrl.label} label={ctrl.label} />
+                <BuildControl 
+                    key={ctrl.label} 
+                    label={ctrl.label} 
+                    added={() => props.ingredientAdded(ctrl.type)} 
+                    removed={() => props.ingredientRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]} />
             )})}
+            <button 
+                className={classes.OrderButton}
+                onClick={props.ordered}
+                disabled={!props.purchasable} >ORDER NOW</button>
         </div>
     );
 };
